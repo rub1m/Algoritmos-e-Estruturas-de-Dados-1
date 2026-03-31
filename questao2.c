ce7100177cd6d2837ee09b9d6169c285ed6f4984
Rubens Guilherme <78000574+rub1m@users.noreply.github.com>
2026-03-31 20:44:01 -0300

    Pessoas

---
+++ questao2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+typedef struct Pessoa{
+    char nome[50];
+    int documento;
+    int idade;
+} Pessoa;
+void adicionarPessoa(Pessoa *p) {
+    printf("Digite o nome: ");
+    scanf("%s", p->nome); 
+    printf("\nDigite o numero do documento: ");
+    scanf("%d", &p->documento);
+    printf("\nDigite a idade: ");
+    scanf("%d", &p->idade);
+}
+void mostrarPessoa(Pessoa *p) {
+    printf("\nDados Pessoais:");
+    printf("\nNome: %s", p->nome);
+    printf("\nDocumento: %d", p->documento);
+    printf("\nIdade: %d anos\n", p->idade);
+}
+void atualizarIdade(Pessoa *p, int outraIdade) {
+    p->idade = outraIdade;
+    printf("\nIdade atualizada");
+}
+void diferencaIdades(Pessoa lista[], int tamanho) {
+    int maior = 0; 
+    int menor = 0; 
+    for (int i = 1; i < tamanho; i++) {
+        if (lista[i].idade > lista[maior].idade) {
+            maior = i;
+        }
+        if (lista[i].idade < lista[menor].idade) {
+            menor = i;
+        }
+    }
+    printf("\nPessoa mais velha: %s", lista[maior].nome);
+    printf("\nPessoa mais nova: %s\n", lista[menor].nome);
+}
+int main() {
+setlocale(LC_ALL, "Portuguese");
+  printf("testando o código, pra não dizer que não rodou heinn ");  
+  Pessoa Lista[2];
+    printf("\nCadastro:\n");
+    for (int i = 0; i < 2; i++) {
+        printf("\nPessoa %d:\n", i + 1);
+        adicionarPessoa(&Lista[i]);
+    }
+    diferencaIdades(Lista, 2);
+    printf("Código grande da mizera!");
+    return 0;
+}

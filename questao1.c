e61a08907456c7f2d7d46dee6aba455de72fbde2
Rubens Guilherme <78000574+rub1m@users.noreply.github.com>
2026-03-31 21:31:38 -0300

    corrigindo

--- questao1.c
+++ questao1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <locale.h>
 typedef struct Funcionario {
     char nome[50];
     float salario;
     int identificador;
     char cargo[50];
-};
-void preencherFuncionario(struct Funcionario* f) {
+} Funcionario;
+void adicionarFuncionario(Funcionario *f) {
     printf("Digite o nome: ");
     scanf("%s", &f->nome);
     printf("Digite o salario: ");
@@ -16,19 +17,22 @@ void preencherFuncionario(struct Funcionario* f) {
     printf("Digite o cargo: ");
     scanf("%s", &f->cargo);
 }
-void imprimirFuncionario(struct Funcionario* f) {
-    printf("Funcionario:\n");
+void mostrarFuncionario(Funcionario *f) {
+    printf("\nFuncionario:\n");
     printf("Nome: %s\n", f->nome);
-    printf("Salário: %f\n", f->salario);
+    printf("Salario: %.2f\n", f->salario);
     printf("Identificador: %d\n", f->identificador);
     printf("Cargo: %s\n", f->cargo);
 }
-void alterarSalario(struct Funcionario *f) {
-    printf("Digite o novo salário: ");
-    scanf("%f", &f->salario);
-    printf("Atualizado!\n");
+void alterarSalario(Funcionario *f) {
+    float novo;
+    printf("Salario atual: R$ %.2f\n", f->salario);
+    printf("Digite o novo salario: ");
+    scanf("%f", &novo);
+    f->salario = novo;
+    printf("Salario atualizado\n");
 }
-void analisarSalarios(struct Funcionario vetor[], int tamanho) {
+void compararSalarios(Funcionario vetor[], int tamanho) {
     int maior = 0;
     int menor = 0;
     for (int i = 0; i < tamanho; i++) {
@@ -39,16 +43,21 @@ void analisarSalarios(struct Funcionario vetor[], int tamanho) {
             menor = i;
         }
     }
-    printf("Maior salário:\n");
+    printf("\nMaior salario:\n");
     printf("Cargo: %s | Salario: R$ %.2f\n", vetor[maior].cargo, vetor[maior].salario);
-    printf("\nMenor salário:\n");
-    printf("Cargo: %s | Salário: R$ %.2f\n", vetor[menor].cargo, vetor[menor].salario);
+    printf("\nMenor salario:\n");
+    printf("Cargo: %s | Salario: R$ %.2f\n", vetor[menor].cargo, vetor[menor].salario);
 }
 int main() {
     setlocale(LC_ALL, "portuguese");
-    struct Funcionario FuncionarioLista[2]
+    printf("testar de novo, corrigindo pela  235235 vez");
+    Funcionario lista[2]; 
     for (int i = 0; i < 2; i++) {
-        preencherFuncionario(&FuncionarioLista[i]);
+        adicionarFuncionario(&lista[i]);
     }
+    for (int i = 0; i < 2; i++) {
+        mostrarFuncionario(&lista[i]);
+    }
+    compararSalarios(lista, 2);
     return 0;
 }

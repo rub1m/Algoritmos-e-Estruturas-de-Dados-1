a6036a5bfaaf97ac3b31a94c25e398423f41b2aa
Rubens Guilherme <78000574+rub1m@users.noreply.github.com>
2026-04-02 10:49:00 -0300

    up
    Implement a program to read an array and print it in reverse order.

---
+++ vetores_questao1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    printf("Tamanho do vetor: ");
+    scanf("%d", &n);
+    int *v = malloc(n * sizeof(int));
+    if (v == NULL){
+      return 1; 
+    }
+    printf("Digite os %d valores:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+    }
+    printf("Ordem Inversa: ");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", v[i]);
+    }
+    free(v);
+    return 0;
+}
